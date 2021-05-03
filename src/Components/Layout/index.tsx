92ea548ea4b812d1abff1c73fe0c5379f379bcdc
maystrova <maystrovakate@gmail.com>
2021-05-03 19:24:04 +0200

    initial

---
+++ src/Components/Layout/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledLayout } from './style'
+
+const Layout = () => {
+    return (
+        <StyledLayout>Hello World!</StyledLayout>
+    )
+}
+
+export {Layout}
---
+++ src/Components/Layout/style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const StyledLayout = styled.div`
+    background-color: red;
+`
+
+export { StyledLayout }

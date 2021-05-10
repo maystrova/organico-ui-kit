f00bfe5f334a1e368b5718ba91679265cb94b649
maystrova <maystrovakate@gmail.com>
2021-05-10 16:15:58 +0200

    Count Component

--- src/Components/Count/index.tsx
+++ src/Components/Count/index.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyledCount, StyledCountButton, StyledCountNumber } from './style'
 import { Icon, ICON_SIZE } from '../Icon'
 import minus from './pics/minus.svg'
 import plus from './pics/plus.svg'
 
-interface CountProps {
-    onAddClick: () => void
-    onRemoveClick: () => void
-    count: number
-}
+interface CountProps {}
 
-const Count = ({ onAddClick, onRemoveClick, count }: CountProps) => {
+const Count = ({}: CountProps) => {
+    const [amount, setAmount] = useState<number>(0)
+
+    const addProduct = () => {
+        let productAmount = amount
+        setAmount(++productAmount)
+    }
+
+    const removeProduct = () => {
+        let productAmount = amount
+        {
+            amount > 0 && setAmount(--productAmount)
+        }
+    }
+
     return (
         <StyledCount>
-            <StyledCountButton onClick={onRemoveClick}>
+            <StyledCountButton onClick={removeProduct}>
                 <Icon size={ICON_SIZE.MEDIUM} src={minus} />
             </StyledCountButton>
-            <StyledCountNumber>{count}</StyledCountNumber>
-            <StyledCountButton onClick={onAddClick}>
+            <StyledCountNumber>{amount}</StyledCountNumber>
+            <StyledCountButton onClick={addProduct}>
                 <Icon size={ICON_SIZE.MEDIUM} src={plus} />
             </StyledCountButton>
         </StyledCount>

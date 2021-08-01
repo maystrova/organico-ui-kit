7c5da645a229cd58225aaf3b016821666e52611e
maystrova <maystrovakate@gmail.com>
2021-08-01 14:21:04 +0300

    ResetPasswordPage structure-feature

--- src/Pages/ResetPasswordPage/index.tsx
+++ src/Pages/ResetPasswordPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { BackToPreviousPage } from 'Components/BackToPreviousPage'
 import { Icon, ICON_SIZE } from 'Components/Icon'
@@ -7,47 +7,82 @@ import { Button, BUTTON_TYPE } from 'Components/Button'
 import { StyledTitledHeader } from 'Pages/WishlistPage/style'
 import {
     StyledRegistrationField,
+    StyledRegistrationFields,
     StyledRegistrationTitle,
 } from 'Pages/NewRegistrationPage/style'
+import {
+    StyledResetPasswordInfo,
+    StyledResetPasswordPage,
+    StyledResetPasswordActions,
+} from './style'
 
 import showPassword from 'Pages/NewRegistrationPage/pics/password-icon.svg'
+import hidePassword from 'Pages/NewRegistrationPage/pics/hide-password.svg'
 
 interface ResetPasswordPageProps {}
 
+interface ResetPassword {
+    title: string
+    icon: string
+}
+
 const ResetPasswordPage = ({}: ResetPasswordPageProps) => {
+    const [isShowPassword, setIsShowPassword] = useState<boolean>(true)
+
+    const icon: string = isShowPassword ? showPassword : hidePassword
+
+    const RESET_PASSWORD: ResetPassword[] = [
+        {
+            title: 'Reset Password',
+            icon: icon,
+        },
+        { title: 'Reset Password Confirmation', icon: icon },
+    ]
+
     return (
-        <div>
+        <StyledResetPasswordPage>
             <StyledTitledHeader>
                 <BackToPreviousPage />
                 <span>Reset Password</span>
             </StyledTitledHeader>
-            <div>
+            <StyledResetPasswordInfo>
                 Please fill in the field below to reset your current password.
-            </div>
-            <div>
-                <StyledRegistrationTitle>New Password</StyledRegistrationTitle>
-                <StyledRegistrationField>
-                    <input type='text' placeholder={'New Password'} />
-                    <Icon size={ICON_SIZE.SMALL} src={showPassword} />
-                </StyledRegistrationField>
-                <StyledRegistrationTitle>
-                    New Password Confirmation
-                </StyledRegistrationTitle>
-                <StyledRegistrationField>
-                    <input
-                        type='text'
-                        placeholder={'New Password Confirmation'}
-                    />
-                    <Icon size={ICON_SIZE.SMALL} src={showPassword} />
-                </StyledRegistrationField>
-                <Button
-                    width={'100%'}
-                    title={'Reset Password'}
-                    type={BUTTON_TYPE.PRIMARY}
-                    onClick={() => {}}
-                />
-            </div>
-        </div>
+            </StyledResetPasswordInfo>
+            <StyledResetPasswordActions>
+                {RESET_PASSWORD.map(field => {
+                    return (
+                        <StyledRegistrationFields>
+                            <StyledRegistrationTitle>
+                                {field.title}
+                            </StyledRegistrationTitle>
+                            <StyledRegistrationField>
+                                <input
+                                    type={isShowPassword ? 'password' : 'text'}
+                                    placeholder={field.title}
+                                />
+                                <button
+                                    onClick={() =>
+                                        setIsShowPassword(!isShowPassword)
+                                    }
+                                >
+                                    <Icon
+                                        size={ICON_SIZE.SMALL}
+                                        src={field.icon}
+                                    />
+                                </button>
+                            </StyledRegistrationField>
+                        </StyledRegistrationFields>
+                    )
+                })}
+            </StyledResetPasswordActions>
+
+            <Button
+                width={'100%'}
+                title={'Reset Password'}
+                type={BUTTON_TYPE.PRIMARY}
+                onClick={() => {}}
+            />
+        </StyledResetPasswordPage>
     )
 }
 
